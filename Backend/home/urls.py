8a4672f375c051a01e08fdae41e161afbe67a3f4
Dev Namdev <devnamdevcse@gmail.com>
2024-03-27 22:52:37 +0530

    update

--- Backend/home/urls.py
+++ Backend/home/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from . import views
-from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from . import views
 urlpatterns = [
     path('', views.index, name='home_index'),
     path('login/', views.user_login, name='login'),
     path('register/', views.user_register, name='register'),
     path('logout/', views.user_logout, name='logout'),
+    path('update/', views.update_user_details, name='update_user_details'),
     path('user/<str:username>/', views.user_dashboard, name='user_dashboard'),
 
 
